1fbf77a0436ecbe7a179ee97d1eaafe0429c5a73
Garcia Berry <garciaberry@outlook.com>
2026-02-13 22:39:18 -0800

    Made Pivot Designation Random

--- main/java/com/example/Main.java
+++ main/java/com/example/Main.java
@@ -1,27 +1,29 @@
 package com.example;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         int[] unsorted = {4, 5, 1, 0};
         int[] answerKey = {0, 1, 4, 5};
-        int[] sorted = Main.quicksort(unsorted);
+        Random rand = new Random();
+        int[] sorted = Main.quicksort(unsorted, rand);
     }
-    public static int[] quicksort(int[] arr)
+    public static int[] quicksort(int[] arr, Random rand)
     {
-        return quicksort(arr, 0, arr.length - 1);
+        return quicksort(arr, 0, arr.length - 1, rand);
     }
-    private static int[] quicksort(int[] arr, int l, int r)
+    private static int[] quicksort(int[] arr, int l, int r, Random rand)
     {
         if(r - l < 1)
         {
             return arr;
         }
-        int pivot = arr[l];
+        int pivot = arr[rand.nextInt(l, r)];
         int lowestRightIndex = r;
         int highestLeftIndex = l;
-        for(int i = l + 1; i <= lowestRightIndex; i++)
+        for(int i = l; i <= lowestRightIndex; i++)
         {
             if(arr[i] > pivot)
             {
@@ -39,8 +41,8 @@ public class Main {
                 highestLeftIndex++;
             }
         }
-        arr = quicksort(arr, l, highestLeftIndex - 1);
-        arr = quicksort(arr, lowestRightIndex + 1, r);
+        arr = quicksort(arr, l, highestLeftIndex - 1, rand);
+        arr = quicksort(arr, lowestRightIndex + 1, r, rand);
         return arr;
     }
 }
--- test/java/com/example/QuickSortTest.java
+++ test/java/com/example/QuickSortTest.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import org.apache.poi.ss.formula.functions.DMin;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.*;
@@ -10,9 +9,10 @@ public class QuickSortTest {
     @DisplayName("Test Quicksort1")
     public void quicksortTest1()
     {
+        Random rand = new Random();
         int[] unsorted = {4, 5, 1, 0, 6, 3, 2, 1};
         int[] answerKey = {0, 1, 1, 2, 3, 4, 5, 6};
-        int[] sorted = Main.quicksort(unsorted);
+        int[] sorted = Main.quicksort(unsorted, rand);
         System.out.println(Arrays.toString(sorted));
         assertTrue(sorted.length == answerKey.length);
         assertArrayEquals(answerKey, sorted);
@@ -21,9 +21,10 @@ public class QuickSortTest {
     @DisplayName("Test Quicksort2")
     public void quicksortTest2()
     {
+        Random rand = new Random();
         int[] unsorted = {0};
         int[] answerKey = {0};
-        int[] sorted = Main.quicksort(unsorted);
+        int[] sorted = Main.quicksort(unsorted, rand);
         System.out.println(Arrays.toString(sorted));
         assertTrue(sorted.length == answerKey.length);
         assertArrayEquals(answerKey, sorted);
@@ -32,11 +33,31 @@ public class QuickSortTest {
     @DisplayName("Test Quicksort3")
     public void quicksortTest3()
     {
+        Random rand = new Random();
         int[] unsorted = {0, 1, 2, 3, 4};
         int[] answerKey = {0, 1, 2, 3, 4};
-        int[] sorted = Main.quicksort(unsorted);
+        int[] sorted = Main.quicksort(unsorted, rand);
         System.out.println(Arrays.toString(sorted));
         assertTrue(sorted.length == answerKey.length);
         assertArrayEquals(answerKey, sorted);
+    }
+    @Test
+    @DisplayName("Large + Random Test")
+    public void quicksortTest4()
+    {
+        Random rand = new Random();
+        for(int run = 0; run < 10000; run++) {
+            int[] unsorted = new int[10000];
+            for(int i = 0; i < unsorted.length; i++) {
+                unsorted[i] = rand.nextInt(0, 10000);
+            }
+            int[] sorted = Main.quicksort(unsorted, rand);
+            System.out.println(Arrays.toString(sorted));
+            int last = -1;
+            for(int i = 0; i < sorted.length; i++) {
+                assertTrue(sorted[i] >= last);
+                last = sorted[i];
+            }
+        }
     }
 }
